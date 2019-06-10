d689253436c06427538336b4b7e9a04ae2e6a5d9
David <d12yes@gmail.com>
2019-06-09 22:17:16 -0500

    Handles SCSS code with .scss extensions; should output a single .css file with a unique hash added to the filename.

--- webpack-project/src/index.js
+++ webpack-project/src/index.js
@@ -1,1 +1,15 @@
+import './styles.scss';
+
 console.log("Webpack entry point!")
+
+// function component() {
+//     const element = document.createElement('div');
+
+//     // Lodash, now imported by this script
+//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//     element.classList.add('hello');
+
+//     return element;
+//   }
+
+//   document.body.appendChild(component());
